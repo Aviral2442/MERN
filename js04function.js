ba2770c066fd28c7066edd51a22e283c53859feb
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-02-19 18:23:56 +0530

    java

--- js04function.js
+++ js04function.js
@@ -1,15 +1,42 @@
-function add( a , b){
-    c = a+b;
+function add(a, b) {
+    c = a + b;
     console.log(c);
 }
 
-add ( 23 ,1);
+add(23, 1);
 
 
-function add( a , b , c , d){
-    c = a+b+c+d;
+function add(a, b, c, d) {
+    c = a + b + c + d;
     console.log(c);
 }
 
-add ( 23 ,1 , 2 , 3);
+add(23, 1, 2, 3);
+
+
+const getAvg = function (m1, m2, m3) {
+    const avg = (m1 + m2 + m3) / 3;
+    // console.log(avg);
+    return avg;
+
+}
+getAvg(34, 56, 78);
+const result = getAvg(34, 56, 78);
+console.log(result);
+
+// by es5
+
+
+const getFact = (n) => {
+    let f = 1;
+    for (let k = 1; k <= n; k++) {
+        f = f * k;
+    }
+    // console.log(f);
+    return f;
+}
+getFact(5)
+
+const getFacs = getFact(10)/getFact(5);
+console.log(getFacs);
 
---
+++ js05arrays.js
@@ -0,0 +1,33 @@
+const arr1 = [745 , 347783.3, 'hello', true, null];
+console.log(arr1);
+
+const fruits = ['apple', 'mango', "orange", 'banana', 'kiwi'];
+// indexing   //also works with string
+console.log(fruits[3]); // element positive
+console.log(fruits.at(-2)); // element negative
+console.log(fruits[1],fruits[4]);
+console.log(fruits.indexOf('banana'));  //index
+console.log(fruits.length); // lenght
+
+// slicing , also works with string
+
+console.log( fruits.slice(1,3));   //also works with string
+console.log( fruits.slice(1,6));   //also works with string
+console.log( fruits.slice(1));   //also works with string
+
+price = "₹87579.87979"
+console.log(price.slice(1));
+
+
+// adding elements
+fruits.push('keela'); //adds at the ends
+console.log(fruits);
+fruits.unshift('papaya') // adding element at first place
+console.log(fruits);
+
+
+// removing elements
+fruits.pop(); // removes at the ends 
+console.log(fruits);
+fruits.shift(); // removes from first
+console.log(fruits);
