5003e9cf3cfd8bd06e5616f55f506017f14db4c4
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-02-21 23:50:37 +0530

    js

--- js06homework.js
+++ js06homework.js
@@ -1,45 +1,105 @@
-// WAP to check if a number is prime
+// // WAP to check if a number is prime
 
-function isPrime(num) {
-    if (num <= 1) return false; 
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false; 
+// function isPrime(num) {
+//     if (num <= 1) return false; 
+//     for (let i = 2; i < num; i++) {
+//         if (num % i === 0) {
+//             return false; 
+//         }
+//     }
+//     return true; 
+// }
+
+// const userInput = 11;
+// console.log(`Is ${userInput} a prime number? `, isPrime(userInput));
+
+
+// // Create a function to find area of a triangle.
+   
+// function triangleArea(base, height) {
+//     return 0.5 * base * height;
+//   }
+   
+//   let baseLength = 10;
+//   let heightLength = 5;
+//   let area = triangleArea(baseLength, heightLength);
+// //   console.log(`The area of the triangle is: ${area}`);
+// console.log(area);
+
+
+// // Create a function to reverse a number.
+
+// function reverseFunction(num) {
+// 	let numStr = num.toString();
+// 	let reversedStr = '';
+// 	for (let i = numStr.length - 1; i >= 0; i--) {
+// 		reversedStr += numStr[i];
+// 	}
+// 	return parseInt(reversedStr);
+// }
+
+// let num = 987654321;
+// let reversedNum = reverseFunction(num);
+
+// console.log(reversedNum);
+
+
+
+// prime number check
+
+let n = 13;
+let prime = true;
+
+for (let i = 2; i < n / 2; i++) {
+    if (n % i === 0) {
+        console.log('not prime');
+        prime = false;
+        break;
+    }
+}
+
+if (prime) {
+    console.log('prime');
+}
+
+const checkPrime = (n) => {
+
+    for (let i = 2; i < n / 2; i++) {
+        if (n % i === 0) {
+            return false;
         }
     }
-    return true; 
+
+    return true;
 }
 
-const userInput = 11;
-console.log(`Is ${userInput} a prime number? `, isPrime(userInput));
-
-
-// Create a function to find area of a triangle.
-   
-function triangleArea(base, height) {
-    return 0.5 * base * height;
-  }
-   
-  let baseLength = 10;
-  let heightLength = 5;
-  let area = triangleArea(baseLength, heightLength);
-//   console.log(`The area of the triangle is: ${area}`);
-console.log(area);
-
-
-// Create a function to reverse a number.
-
-function reverseFunction(num) {
-	let numStr = num.toString();
-	let reversedStr = '';
-	for (let i = numStr.length - 1; i >= 0; i--) {
-		reversedStr += numStr[i];
-	}
-	return parseInt(reversedStr);
+for(let i=100; i<=600; i++){
+    if( checkPrime(i) ){
+        console.log(i);
+    }
 }
 
-let num = 987654321;
-let reversedNum = reverseFunction(num);
+let [a, b] = [0, 1];
+let c = a+b;
+console.log(a);
+console.log(b);
+while(c <= 100){
+    console.log(c);
+    [a, b] = [b, c];
+    c = a+b;
+}
 
-console.log(reversedNum);
+// reverse a number
 
+let num = 456767;
+let reverse = 0;
+
+while(num > 0){
+    // console.log(num);
+    // console.log(reverse);
+    let d = num % 10;
+    reverse = reverse * 10 + d;
+    num = parseInt(num/10);
+}
+
+console.log(reverse);
---
+++ js07arrque.js
@@ -0,0 +1,80 @@
+// WAP to get sum of all  elements in array 
+
+const nums = [34, 28, 100, 29, 35, 77, 38];
+
+// method1
+let sum = 0;
+for (i of nums) {
+    sum = sum + i;
+}
+console.log(sum);
+
+// method2
+let sum2 = 0;
+nums.forEach((a) => { sum2 = sum2 + a; });
+console.log(sum2);
+
+
+// get some of all elements on even indexes
+
+const nums1 = [34, 28, 100, 29, 35, 77, 38];
+
+// method1
+let sum1 = 0;
+for (i of nums1) {
+    if (i % 2 === 0) {
+        sum1 = sum1 + i;
+    }
+}
+console.log(sum1);
+
+// method2
+let sum3 = 0;
+nums1.forEach((a, i) => {
+    if (a % 2 === 0) {
+        sum3 = sum3 + a;
+        console.log(a, i);
+    }
+});
+console.log(sum3);
+
+
+
+
+
+// square of all number
+
+for (let i of nums1) {
+    console.log(i ** 2);
+}
+
+console.log("----------------------------");
+
+// cube of all number
+
+for (let i of nums1) {
+    console.log(i ** 3);
+}
+
+console.log("----------------------------");
+console.log("----------------------------");
+
+// get some of all elements on even elements
+
+const numss = [34, 28, 100, 29, 35, 77, 38];
+
+let sums = 0;
+for (let i of numss) {
+    // console.log(i);
+    if (i%2 === 0) {
+        sums += i;
+  }
+}
+
+    console.log("----------------------------");
+
+
+numss.forEach((a) => { if (a % 2 === 0) { sums += a; }}); 
+console.log(sums);
+
+
