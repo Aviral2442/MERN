c65bb00fcb91240eff453371fabd23e34f68ba05
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-02-25 14:34:52 +0530

    js

---
+++ js10practice.js
@@ -0,0 +1,17 @@
+const arr = [ 24 , 'Aviral '];
+console.log(arr);
+
+arr.push('Srivastava');
+console.log(arr);
+
+arr.unshift(2004);
+console.log(arr);
+
+// arr.pop(); 
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+arr.shift();
+console.log(arr);
